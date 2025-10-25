0efe054088caac84ba6bc0c9a829feaf123d6a18
Sara-Rubaya <sararubaya4800@gmail.com>
2025-10-25 20:22:04 +0600

    fixed navbar

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const HowItWorks = () => {
   return (
-    <section className="py-12 ">
-      <div className="max-w-6xl mx-auto px-4 text-center">
+    <section className="py-12 w-full ">
+      <div className="w-full px-20 text-center">
         <h2 className="text-3xl font-bold text-teal-600 mb-6">How It Works</h2>
         <div className="grid gap-6 md:grid-cols-3">
           {/* Step 1 */}
           <div className="p-6 border rounded-xl shadow hover:shadow-md transition bg-base-100">
             <h3 className="text-xl font-semibold text-accent mb-2">1. Browse Packages</h3>
-            <p className="">
+            <p>
               Explore a wide variety of travel destinations and curated tour packages that suit your interests and budget.
             </p>
           </div>
@@ -17,7 +17,7 @@ const HowItWorks = () => {
           {/* Step 2 */}
           <div className="p-6 border rounded-xl shadow hover:shadow-md transition bg-base-100">
             <h3 className="text-xl font-semibold text-accent mb-2">2. Book Easily</h3>
-            <p className="">
+            <p>
               Secure your spot with just a few clicks. Our booking system is fast, simple, and safe.
             </p>
           </div>
@@ -25,7 +25,7 @@ const HowItWorks = () => {
           {/* Step 3 */}
           <div className="p-6 border rounded-xl shadow hover:shadow-md transition bg-base-100">
             <h3 className="text-xl font-semibold text-accent mb-2">3. Enjoy Your Trip</h3>
-            <p className="">
+            <p>
               Pack your bags and relax. We handle the details so you can enjoy every moment of your journey.
             </p>
           </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -36,7 +36,7 @@ useEffect(() => {
   </>
 
     return (
-        <div className="navbar shadow-sm bg-base-100 px-6">
+        <div className="navbar shadow-sm bg-base-100 px-6 sticky top-0 z-50">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
